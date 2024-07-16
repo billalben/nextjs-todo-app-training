dd218a544e23c7438068575356a6bd9a31a61d46
Benzazoua <billalben252@gmail.com>
2024-07-16 22:30:27 +0100

    Auth & Form Validation & Todo Table

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { getTodoListAction } from "@/actions/todo.actions";
+import { getUserTodoListAction } from "@/actions/todo.actions";
+import AddTodoForm from "@/components/AddTodoForm";
+import TodosTable from "@/components/TodoTable";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  // get todo list
-  const todoList = await getTodoListAction();
+  const { userId }: { userId: string | null } = auth();
+
+  if (!userId) return null;
+
+  const todos = await getUserTodoListAction({ userId });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Dialog>
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="mr-1 h-4 w-4" />
-            Click me
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                defaultValue="Billal Benzazoua"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input
-                id="username"
-                defaultValue="@billal"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog> */}
-
-      <ul>
-        {todoList.map((todo) => (
-          <li
-            key={todo.id}
-            className="bg-gray-100 p-4 rounded-md shadow-md w-flil sm:w-[425px]"
-          >
-            <h2 className="text-black text-lg font-semibold">{todo.title}</h2>
-            <p className="text-gray-600">{todo.body}</p>
-          </li>
-        ))}
-      </ul>
+    <main className="container">
+      <div className="mx-auto flex w-full lg:w-3/4 flex-col justify-center space-y-4 mt-10">
+        <AddTodoForm userId={userId} />
+        <TodosTable todos={todos} />
+      </div>
     </main>
   );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,54 +3,7 @@ import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     email: "contact@billal.me",
-  //     name: "Billal Benzazoua",
-  //   }
-  // });
-  /**
-   * generate fake data todo list for Todo model
-   * */
-
-  // await prisma.todo.createMany({
-  //   data: Array.from({ length: 5 }).map(() => ({
-  //     title: faker.lorem.words(2),
-  //     body: faker.lorem.sentence(10),
-  //   })),
-  // });
-
-  // await prisma.todo.delete({
-  //   where: {
-  //     id: "6694fcdd27a7c0353586e896",
-  //   },
-  // });
-
-  // await prisma.todo.update({
-  //   where: {
-  //     id: "6694fedba8f5f1d191a81e54",
-  //   },
-  //   data: {
-  //     title: "Updated title",
-  //     body: "Updated body",
-  //     completed: true,
-  //   }
-  // });
-
-  // await prisma.user
-  //   .findUnique({
-  //     where: {
-  //       id: "6694fbe2fee7292a537b6450",
-  //     },
-  //     include: {
-  //       todos: true,
-  //     },
-  //   })
-  //   .then((user) => {
-  //     console.log(user);
-  //   });
-}
+async function main() {}
 
 main()
   .catch(async (e) => {
